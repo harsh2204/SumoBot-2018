2954b4f39535877384e698a777409314aec041b1
harsh2204 <harshgupta2204@live.com>
2018-03-25 05:50:09 -0400

    STUASDK

--- ultraSonic_Code_2/ultraSonic_Code_2.ino
+++ ultraSonic_Code_2/ultraSonic_Code_2.ino
@@ -2,41 +2,16 @@
 //Instantiate a new advanced serial object with 500 transmit slots
 
 
-#define trigPin 2 // GREY
-#define echoPin 3 // PURPLE
-#define trigPin2 7
+#define trigPin 2 // Front Left
+#define echoPin 3 
+#define trigPin2 7 //Front Right
 #define echoPin2 8
-#define trigPinL 11
+#define trigPinL 11 //Left
 #define echoPinL 12
-#define trigPinR 0
+#define trigPinR 0 //Right
 #define echoPinR 1
 
-//5 & 6 are free
 
-// ULTRASONICS
-// BACK:
-// 6 Grey, 7 Purple
-
-//Front Right:
-// 2 Grey, 3 Purple
-
-
-//Front Left:cd p
-// 4 Grey, 5 Purple
-
-//LEFT:
-// 9 Grey, 8 Purple
-
-//RIGHT:
-
-
-//int prevDist = 0;
-//int prevDist2 = 0;
-//int prevLeftAngle = 0;
-//int prevRightAngle = 0;
-//int prevFarAngle = 0;
-//int update = 0;
-//int update2 = 0;
 long distance, distance2, distanceR, distanceL;
 
 void setup() {
@@ -86,7 +61,7 @@ boolean peripheralView(int side){
   if(side){ //right
     return (distanceR>=7 && distanceR<=80);
   }else{
-    return (distanceL>=7 && distanceL<=80);
+    return (distanceLd>=7 && distanceL<=80);
   }
 }
 
@@ -98,41 +73,7 @@ void loop() {
   distanceL = pingUltra(trigPinL, echoPinL);
   distanceR = pingUltra(trigPinR, echoPinR);
   printDist();
-//  printView();
+  printView();
   
 }
 
-//float calcX(float distLeft, float distRight){
-//  float x = 20;
-//  float thetaLeft = acos((sq(distRight) + sq(4) - sq(distLeft))/(2*distRight*4));
-//  float thetaRight = acos((sq(distLeft) + sq(4) - sq(distRight))/(2*distLeft*4));
-//  if(distRight > distLeft){
-//    //float y = distRight*sin(thetaRight);
-//    x = distRight*cos(thetaRight);
-//    
-//  }
-// 
-//  else{
-//    //float y = distLeft*sin(thetaLeft);
-//    x = -distLeft*cos(thetaLeft);
-//  }
-//  return x;
-//  
-//}
-//
-//float calcY(float distLeft, float distRight){
-//  float y = 10;
-//  float thetaLeft = acos((sq(distRight) + sq(4) - sq(distLeft))/(2*distRight*4));
-//  float thetaRight = acos((sq(distLeft) + sq(4) - sq(distRight))/(2*distLeft*4));
-//  if(distRight > distLeft){
-//    y = distRight*sin(thetaRight);
-//    //float x = distRight*cos(thetaRight);
-//  }
-// 
-//  else{
-//    y = distLeft*sin(thetaLeft);
-//    //float x = distLeft*cos(thetaLeft);
-//  }
-//  return y;
-//  
-//}
